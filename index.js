46764a9f20e371a5c29be1b739904cfde4f0709d
Hira Irfan <162793914+hirairfan04@users.noreply.github.com>
2024-05-28 08:12:43 +0500

    program files

---
+++ index.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let choice = await inquirer.prompt([
+    {
+        message: 'Choose one option:',
+        name: 'options',
+        type: 'list',
+        choices: ['Hour', 'Minutes', 'Seconds']
+    }
+]);
+if (choice.options == 'Hour') {
+    let answ = await inquirer.prompt([
+        {
+            message: 'Enter time in hours: ',
+            name: 'hr',
+            type: 'number'
+        }
+    ]);
+    let x = answ.hr * 3600;
+    console.log(`${answ.hr} hours = ${x} seconds`);
+    function hR() {
+        for (let j = x; j >= 0; j--) {
+            setTimeout(() => {
+                console.log(j);
+            }, (x - j) * 1000);
+        }
+    }
+    hR();
+}
+else if (choice.options == 'Minutes') {
+    let ans = await inquirer.prompt([
+        {
+            message: 'Enter time in minutes: ',
+            name: 'min',
+            type: 'number'
+        }
+    ]);
+    let x = ans.min * 60;
+    console.log(`${ans.min} minutes = ${x} seconds`);
+    function minute() {
+        for (let j = x; j >= 0; j--) {
+            setTimeout(() => {
+                console.log(j);
+            }, (x - j) * 1000);
+        }
+    }
+    minute();
+}
+else if (choice.options == 'Seconds') {
+    let answer = await inquirer.prompt([
+        {
+            name: 'time',
+            message: 'Enter time:',
+            input: 'number'
+        }
+    ]);
+    function timer() {
+        for (let i = answer.time; i >= 0; i--) {
+            setTimeout(() => {
+                console.log(i);
+            }, (answer.time - i) * 1000);
+        }
+    }
+    timer();
+}
